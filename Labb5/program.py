4165d148b725f2fbc09dc5653f4a4cb895c8c762
Volkan <30434639+Vookiee@users.noreply.github.com>
2020-03-04 22:19:40 +0100

    Add files via upload

--- Labb5/program.py
+++ Labb5/program.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import requests , json
+import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('bilar.csv', sep=';')
-print(data)
+data = pd.read_csv('Bensin1.csv', sep=';')
+#data.set_index('län')
+år = data[['Sthlm','Uppsala','Södmln','Jkpg','Kalmar','Gotlands','Blekinge','Skåne','Örebro','Dalarna','Jämtlands','Västerbtn','Norrbtn']]
+lan = data['år']
+plt.plot(lan,år)
+plt.ylabel('Bensinbilar')
+plt.xlabel('Län')
+plt.show()
+#print(data)
